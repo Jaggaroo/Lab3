f1d11ed5e4eced364005bb185852f141fcedfbec
John Green <jagg00001@yahoo.ca>
2020-02-11 20:27:11 -0400

    Second commit. Requirements 2 to 8 completed.

--- src/main/java/com/inft4001/StringCalculator.java
+++ src/main/java/com/inft4001/StringCalculator.java
@@ -1,28 +1,51 @@
 package com.inft4001;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 
-    public static void add(String numbers) {
-        String[] numbersArray = numbers.split(",");
-        if (numbersArray.length > 2) {
-            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-        } else {
-            for (String number : numbersArray) {
-                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
-            }
+    public static int add(final String numbers) {
+        String delimiter = ",|n";
+        String numbersWithoutDelimiter = numbers;
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("//") + 2;
+            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("n") + 1);
         }
-    } // END public static void add(String s)
+        return add(numbersWithoutDelimiter, delimiter);
+    }
 
-    public static void subtract(String numbers) {
-        String[] numbersArray = numbers.split(",");
-        if (numbersArray.length > 2) {
-            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-        } else {
-            for (String number : numbersArray) {
-                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
+    private static int add(final String numbers, final String delimiter) {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        List negativeNumbers = new ArrayList();
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) {
+                int numberInt = Integer.parseInt(number.trim());
+                if (numberInt < 0) {
+                    negativeNumbers.add(numberInt);
+                } else if (numberInt <= 1000) {
+                    returnValue += numberInt;
+                }
             }
         }
-    } // END public static void subtract(String s)
+        if (negativeNumbers.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return returnValue;
+    } // END public static void add(String numbers)
+
+//    public static void subtract(String numbers) {
+//        String[] numbersArray = numbers.split(",");
+//        if (numbersArray.length > 2) {
+//            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+//        } else {
+//            for (String number : numbersArray) {
+//                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
+//            }
+//        }
+//    } // END public static void subtract(String s)
 
 } // END class StringCalculator
